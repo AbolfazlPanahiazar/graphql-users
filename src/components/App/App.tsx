a1ab403f48be0f930cce4d5f409653ef584c4a21
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-07-24 14:09:32 +0430

    Add apollo/clien  graphql configurations

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,45 @@
+import { FC, ReactElement } from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
 import Router from "components/Router/Router";
 
-function App() {
+// error catching
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message }) => {
+      console.log(`GraphQL error ${message}`);
+    });
+  }
+});
+
+// create link
+const link = from([
+  errorLink,
+  new HttpLink({
+    uri: process.env.REACT_APP_GRAPHQL_URI,
+  }),
+]);
+
+// crate apollo client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+  headers: {
+    "app-id": `${process.env.REACT_App_GRAPHQL_AUTH_HEADER}`,
+  },
+});
+
+const App: FC = (): ReactElement => {
   return (
-    <div className="flex justify-center bg-gray-800 min-h-screen">
-      <div className="w-full max-w-500">
-        <Router />
+    <ApolloProvider client={client}>
+      <div className="flex justify-center bg-gray-800 min-h-screen">
+        <div className="w-full max-w-500">
+          <Router />
+        </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
